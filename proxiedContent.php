7d9fb62f63cf7105da5e57a441945f5adc55d599
Bastian Hofmann <bhofmann@vz.net>
2011-08-08 13:06:00 +0200

    proxied content with data pipelining

--- proxiedContent.php
+++ proxiedContent.php
@@ -1,4 +1,32 @@
 <?php
+//require the php OAuth library
+require_once "lib/oauth.php";
+
+class MyOAuthSignatureMethod_RSA_SHA1 extends OAuthSignatureMethod_RSA_SHA1 {
+	protected function fetch_public_cert(&$request) {
+	    $s = curl_init();
+		curl_setopt($s,CURLOPT_URL,$_GET['xoauth_signature_publickey']);
+		curl_setopt($s, CURLOPT_RETURNTRANSFER, 1);
+		$cert = curl_exec($s);
+		curl_close($s);
+		return $cert;
+	}
+	protected function fetch_private_cert(&$request) {
+		return;
+	}
+}
+
+$request = OAuthRequest::from_request();
+$server = new MyOAuthSignatureMethod_RSA_SHA1();
+
+
+$return = $server->check_signature($request, null, null, $_GET['oauth_signature']);
+
+if (! $return) {
+	die('invalid signature');
+}
+
+$data = json_decode(file_get_contents('php://input'), true);
 
 ?>
 <script xmlns:os="http://ns.opensocial.org/2008/markup" type="text/os-data">
@@ -7,3 +35,5 @@
 <script type="text/os-template" xmlns:os="http://ns.opensocial.org/2008/markup" require="Viewer">
     Hello ${Viewer.name.givenName} <b>${Viewer.name.familyName}</b>
 </script>
+
+<p>Hello <?= $data[0]['result']['displayName'] ?> from Proxied Content.</p>
