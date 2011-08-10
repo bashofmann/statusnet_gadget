ed7627f2ab1a3c41e0c2c4d944284f2455ae16dd
Bastian Hofmann <bhofmann@vz.net>
2011-08-10 22:53:44 +0200

    added proxied oauth request with templating

--- proxiedContent.php
+++ proxiedContent.php
@@ -37,3 +37,46 @@ $data = json_decode(file_get_contents('php://input'), true);
 </script>
 
 <p>Hello <?= $data[0]['result']['displayName'] ?> from Proxied Content.</p>
+
+<script type="text/os-template" xmlns:os="http://ns.opensocial.org/2008/markup" require="feed" autoUpdate="true">
+    <ul>
+        <li repeat="${feed}">
+            ${Cur.text} (${Cur.created_at}, ${Cur.user.name})
+        </li>
+    </ul>
+</script>
+
+<script type="text/javascript">
+    function loadFeed() {
+        var fetchData = function() {
+            var params = {};
+            params[gadgets.io.RequestParameters.AUTHORIZATION]=gadgets.io.AuthorizationType.OAUTH;
+            params[gadgets.io.RequestParameters.CONTENT_TYPE] = gadgets.io.ContentType.JSON;
+            params[gadgets.io.RequestParameters.OAUTH_SERVICE_NAME]='statusnet';
+            gadgets.io.makeRequest('http://status.net.xyz:8061/index.php/api/statuses/home_timeline.json', function(response) {
+
+                if (response.oauthApprovalUrl) {
+                    var popup = new gadgets.oauth.Popup(
+                        response.oauthApprovalUrl,
+                        'width=400&height=400',
+                        function() { },
+                        function() {
+                            fetchData();
+                        }
+                        );
+
+                    popup.onClick_();
+                } else {
+                    console.log(response);
+                    opensocial.data.DataContext.putDataSet('feed', response.data);
+                }
+            }, params);
+        }
+
+        fetchData();
+    }
+    
+    gadgets.util.registerOnLoadHandler(function() {
+        loadFeed();
+    });
+</script>
